0b7a1b31d7f7ffc8e40d75cffecabf7c13f86f56
ismael <ismael@ismaels-MacBook-Pro.local>
2020-11-23 20:09:14 -0300

    search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import  {useRouter} from 'next/router'
+import {Document} from 'prismic-javascript/types/documents'
+import Prismic from 'prismic-javascript'
+import PrismicDOM from 'prismic-dom'
+import { GetServerSideProps } from 'next';
+import { client } from '~/lib/prismic';
+import Link from 'next/link'
+interface SearchProps{
+  searchResults:Document[]
+}
+const Search = ({searchResults}:SearchProps) => {
+  const [search, setSearch] = useState('')
+  const router = useRouter()
 
-// import { Container } from './styles';
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault()
+    router.push(`/search?q=${encodeURIComponent(search)}`)
+  },[search])
 
-const Search: React.FC = () => {
-  return <div ><h1>search</h1></div>
+
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e=>setSearch(e.target.value)}/>
+        <button type='submit'>Search</button>
+      </form>
+      <ul>
+        {searchResults.map(recommendedProduct=>{
+          return (
+            <li key={recommendedProduct.id}>
+              <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                <a>
+                  {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Search;
+
+export const getServerSideProps: GetServerSideProps<SearchProps> = async (context) => {
+  const {q} = context.query
+
+  if(!q){
+    return {props:{searchResults:[] as Document[]}}
+  }
+
+  const searchResults = await client().query([
+    Prismic.Predicates.at('document.type', 'product'),
+    Prismic.Predicates.fulltext('my.product.title', String(q))
+  ])
+
+  return {
+    props:{searchResults:searchResults.results}
+  }
+
+
+}
